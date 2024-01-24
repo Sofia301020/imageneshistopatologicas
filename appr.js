d4c71ee074f608494f65360e04138f1b0e76fae1
Sofia <sofia.lorena@hotmail.com>
2024-01-23 23:25:12 -0500

    .env

--- appr.js
+++ appr.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
@@ -8,8 +9,9 @@ console.log('Peticion recibida')
 res.send('<h1>Hola mundo </h1>')
 })
 
+const puerto = process.env.puerto || 4000
 
-app.listen(4000, () => {
-   console.log('Servidor escuchand en el puerto 4000')
+app.listen(puerto, () => {
+   console.log('Servidor escucha en el puerto 4000')
 
   }) 
